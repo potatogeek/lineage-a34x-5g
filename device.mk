fcba7230da9a524253704689478aabd4c4a74154
Megh Rana <meghthedev@gmail.com>
2025-05-07 17:42:43 +0530

    a34x: SOONG_NAMESPACES: samsung_ext -> samsung

--- device.mk
+++ device.mk
@@ -115,7 +115,7 @@ PRODUCT_SHIPPING_API_LEVEL := 31
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/samsung_ext
+    hardware/samsung
 
 # VNDK API
 PRODUCT_TARGET_VNDK_VERSION := 31
